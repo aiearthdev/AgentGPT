ab8da82a312ae99b4d69085b3abd6050313d580f
aiearthdev <aiearthhub@proton.me>
2023-11-25 12:36:41 +0000

    build pltform

--- platform/reworkd_platform/settings.py
+++ platform/reworkd_platform/settings.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
     """
 
     # Application settings
-    host: str = "127.0.0.1"
+    host: str = "0.0.0.0"
     port: int = 8000
     workers_count: int = 1
     reload: bool = True
