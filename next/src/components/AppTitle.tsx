dad6acad47d96874b2922d508022f3d8f4ccd2bd
aiearthdev <aiearthhub@proton.me>
2023-12-04 23:41:16 +0800

    Update AppTitle.tsx

--- next/src/components/AppTitle.tsx
+++ next/src/components/AppTitle.tsx
@@ -6,14 +6,21 @@ const AppTitle = () => {
   return (
     <div id="title" className="relative flex flex-col items-center">
       <div className="flex flex-row items-start">
-          
         <span
           className="text-4xl font-bold text-slate-12 xs:text-5xl sm:text-7xl"
           style={{
             textShadow: "0px 5px 5px rgba(0, 0, 0, 0.1)",
           }}
         >
-          智慧地球（AI·Earth）社区<br>
+          智慧地球（AI·Earth）社区
+        </span>
+        <br>
+        <span
+          className="text-4xl font-bold text-slate-12 xs:text-5xl sm:text-7xl"
+          style={{
+            textShadow: "0px 5px 5px rgba(0, 0, 0, 0.1)",
+          }}
+        >
           AgentGPT
         </span>
       </div>
